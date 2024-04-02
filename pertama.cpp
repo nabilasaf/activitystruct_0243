52166cb57f90d8d56559ead449babb6e88043151
nabilasaf <nabilasafitri861@gmail.com>
2024-04-02 15:00:04 +0700

    menampilkan_1

--- pertama.cpp
+++ pertama.cpp
@@ -10,8 +10,24 @@ struct Mahasiswa {
     string alamat;
     string umur;
 };
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    Mahasiswa mhs;
+    cout << "nomor mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "nama mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "alamat mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "umur mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n nim : " << mhs.nim;
+    cout << "\n nama : " << mhs.nama;
+    cout << "\n alamat : " << mhs.alamat;
+    cout << "\n umur : " << mhs.umur;
+
 }
 
